bb81125015d246e811623bb8a0bf230502d3c25b
wkamyk <witoldkaminski07@gmail.com>
2024-01-20 22:54:11 +0100

    .

--- konwerter_jednostek.py
+++ konwerter_jednostek.py
@@ -7,7 +7,11 @@ while True:
 
     wybor = int(input('Wpisz numer twojego wyboru: '))
 
-    if wybor == 1:
+    if wybor == 0:
+        print('Zamykanie programu...')
+        break
+
+    elif wybor == 1:
         print('=====TEMPERATURA=====')
         print('Spis numerow zamian:')
         print('1 - stopnie Celsjusza --> stopnie Kelwina')
@@ -17,31 +21,31 @@ while True:
         print('5 - stopnie Fahrenheita --> stopnie Celsjusza')
         print('6 - stopnie Fahrenheita --> stopnie Kelwina')
 
-        wybor = int(input('Wpisz numer twojego wyboru: '))
-        ilosc = int(input('Podaj ilosc jesdostek jaka chcesz przekonwetwoac: '))
+        wybor_temperatura = int(input('Wpisz numer twojego wyboru: '))
+        ilosc_temperatura = int(input('Podaj ilosc jednostek jaką chcesz przekonwertować: '))
 
-        if wybor == 1:
-            wynik = ilosc+273.15
-            print(ilosc, '°C to', wynik, 'K')
-        elif wybor == 2:
-            wynik = ilosc*2+32
-            print(ilosc, '°C to', wynik, '°F')
-        elif wybor == 3:
-            wynik = (ilosc-273.15)*1.8+32
-            print(ilosc, 'K to', wynik, '°F')
-        elif wybor == 4:
-            wynik = ilosc-273.15
-            print(ilosc, 'K to', wynik, '°C')
-        elif wybor == 5:
-            wynik = (ilosc-32)/1.8
-            print(ilosc, '°F to', wynik, '°C')
-        elif wybor == 6:
-            wynik = (ilosc+459.67)*(5/9)
-            print(ilosc, '°F to', wynik, 'K')
+        if wybor_temperatura == 1:
+            wynik = ilosc_temperatura + 273.15
+            print(ilosc_temperatura, '°C to', wynik, 'K')
+        elif wybor_temperatura == 2:
+            wynik = ilosc_temperatura * 1.8 + 32
+            print(ilosc_temperatura, '°C to', wynik, '°F')
+        elif wybor_temperatura == 3:
+            wynik = (ilosc_temperatura - 273.15) * 1.8 + 32
+            print(ilosc_temperatura, 'K to', wynik, '°F')
+        elif wybor_temperatura == 4:
+            wynik = ilosc_temperatura - 273.15
+            print(ilosc_temperatura, 'K to', wynik, '°C')
+        elif wybor_temperatura == 5:
+            wynik = (ilosc_temperatura - 32) / 1.8
+            print(ilosc_temperatura, '°F to', wynik, '°C')
+        elif wybor_temperatura == 6:
+            wynik = (ilosc_temperatura + 459.67) * (5 / 9)
+            print(ilosc_temperatura, '°F to', wynik, 'K')
         else:
             print('Nie ma takiego wyboru!')
 
-    if wybor == 2:
+    elif wybor == 2:
         print('=====POWIERZCHNIA=====')
         print('Spis numerow zamian:')
         print('1 - mm² --> cm²')
@@ -57,45 +61,44 @@ while True:
         print('11 - dm² --> cm²')
         print('12 - cm² --> mm²')
 
-        wybor = int(input('Wpisz numer twojego wyboru: '))
-        ilosc = int(input('Podaj ilosc jesdostek jaka chcesz przekonwetwoac: '))
+        wybor_powierzchnia = int(input('Wpisz numer twojego wyboru: '))
+        ilosc_powierzchnia = int(input('Podaj ilość jednostek jaką chcesz przekonwertować: '))
 
-        if wybor == 1:
-            wynik = ilosc*0.01
-            print(ilosc, 'mm² to', wynik, 'cm²')
-        elif wybor == 2:
-            wynik = ilosc*0.01
-            print(ilosc, 'cm² to', wynik, 'dm²')
-        elif wybor == 3:
-            wynik = ilosc*0.01
-            print(ilosc, 'dm² to', wynik, 'm²')
-        elif wybor == 4:
-            wynik = ilosc*0.01
-            print(ilosc, 'm² to', wynik, 'a')
-        elif wybor == 5:
-            wynik = ilosc*0.01
-            print(ilosc, 'a to', wynik, 'ha')
-        elif wybor == 6:
-            wynik = ilosc*0.01
-            print(ilosc, 'ha to', wynik, 'km²')
-        elif wybor == 7:
-            wynik = ilosc*100
-            print(ilosc, 'km² to', wynik, 'ha')
-        elif wybor == 8:
-            wynik = ilosc*100
-            print(ilosc, 'ha to', wynik, 'a')
-        elif wybor == 9:
-            wynik = ilosc*100
-            print(ilosc, 'a to', wynik, 'm²')
-        elif wybor == 10:
-            wynik = ilosc*100
-            print(ilosc, 'm² to', wynik, 'dm²')
-        elif wybor == 11:
-            wynik = ilosc*100
-            print(ilosc, 'dm² to', wynik, 'cm²')
-        elif wybor == 12:
-            wynik = ilosc*100
-            print(ilosc, 'cm² to', wynik, 'mm²')
+        if wybor_powierzchnia == 1:
+            wynik = ilosc_powierzchnia * 0.01
+            print(ilosc_powierzchnia, 'mm² to', wynik, 'cm²')
+        elif wybor_powierzchnia == 2:
+            wynik = ilosc_powierzchnia * 0.01
+            print(ilosc_powierzchnia, 'cm² to', wynik, 'dm²')
+        elif wybor_powierzchnia == 3:
+            wynik = ilosc_powierzchnia * 0.01
+            print(ilosc_powierzchnia, 'dm² to', wynik, 'm²')
+        elif wybor_powierzchnia == 4:
+            wynik = ilosc_powierzchnia * 0.01
+            print(ilosc_powierzchnia, 'm² to', wynik, 'a')
+        elif wybor_powierzchnia == 5:
+            wynik = ilosc_powierzchnia * 0.01
+            print(ilosc_powierzchnia, 'a to', wynik, 'ha')
+        elif wybor_powierzchnia == 6:
+            wynik = ilosc_powierzchnia * 0.01
+            print(ilosc_powierzchnia, 'ha to', wynik, 'km²')
+        elif wybor_powierzchnia == 7:
+            wynik = ilosc_powierzchnia * 100
+            print(ilosc_powierzchnia, 'km² to', wynik, 'ha')
+        elif wybor_powierzchnia == 8:
+            wynik = ilosc_powierzchnia * 100
+            print(ilosc_powierzchnia, 'ha to', wynik, 'a')
+        elif wybor_powierzchnia == 9:
+            wynik = ilosc_powierzchnia * 100
+            print(ilosc_powierzchnia, 'a to', wynik, 'm²')
+        elif wybor_powierzchnia == 10:
+            wynik = ilosc_powierzchnia * 100
+            print(ilosc_powierzchnia, 'm² to', wynik, 'dm²')
+        elif wybor_powierzchnia == 11:
+            wynik = ilosc_powierzchnia * 100
+            print(ilosc_powierzchnia, 'dm² to', wynik, 'cm²')
+        elif wybor_powierzchnia == 12:
+            wynik = ilosc_powierzchnia * 100
+            print(ilosc_powierzchnia, 'cm² to', wynik, 'mm²')
         else:
             print('Nie ma takiego wyboru!')
-    wybor=int(input('jesli chcesz kontynuowac wpisz 1, jesli nie wpisz 0: '))
